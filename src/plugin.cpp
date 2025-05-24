bbfbf55b5e10521dd6d4090cd0ba3f72c1c494e1
Manuel Schneider <manuelschneid3r@gmail.com>
2025-05-24 02:59:59 +0200

    Avoid runtime conversion from utf8 for QString construction

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -8,23 +8,24 @@
 #include <chrono>
 #include <thread>
 ALBERT_LOGGING_CATEGORY("debug")
+using namespace Qt::Literals;
 using namespace albert;
 using namespace std;
 using namespace util;
 
-static auto icon = {QStringLiteral("qsp:SP_MessageBoxWarning")};
+static auto icon = {u"qsp:SP_MessageBoxWarning"_s};
 
 Plugin::Plugin() { DEBG << "'Debug' created."; }
 
 Plugin::~Plugin() { DEBG << "'Debug' destroyed."; }
 
-QString Plugin::synopsis(const QString &) const { return "debug-debug-debug-debug"; }
+QString Plugin::synopsis(const QString &) const { return u"debug-debug-debug"_s; }
 
 bool Plugin::allowTriggerRemap() const { return false; }
 
 void Plugin::handleTriggerQuery(Query &query)
 {
-    if (query.string() == QStringLiteral("busy"))
+    if (query.string() == u"busy"_s)
     {
         for(int i = 0; query.isValid() && i < 3; ++i)
         {
@@ -33,38 +34,31 @@ void Plugin::handleTriggerQuery(Query &query)
                     return;
                 this_thread::sleep_for(chrono::milliseconds(10));
             }
-            query.add(
-                StandardItem::make(
-                    {}, QString("Item #%1").arg(i), QString("Wow, Item #%1").arg(i), icon,
-                    {
-                        {"Debug","Open website", [](){ openWebsite(); }}
-                    }
-                )
-            );
+            query.add(StandardItem::make({},
+                                         u"Item #%1"_s.arg(i),
+                                         u"Wow, Item #%1"_s.arg(i),
+                                         icon,
+                                         {{u"Debug"_s, u"Open website"_s, []() { openWebsite(); }}}));
         }
         return;
     }
 
-    if (QStringLiteral("notification").startsWith(query.string()))
+    if (u"notification"_s.startsWith(query.string()))
     {
-        query.add(StandardItem::make(
-            {}, "Memleaking notification", "Leaks memory", icon,
-            {
-                {
-                    "Debug", "Open website",
-                    [](){
-                        auto *n = new Notification;
-                        n->setTitle("Memory leak");
-                        n->setText("Ok.");
-                        n->send();
-                    }
-                }
-            }
-        ));
+        query.add(StandardItem::make({},
+                                     u"Memleaking notification"_s,
+                                     u"Leaks memory"_s,
+                                     icon,
+                                     {{u"Debug"_s, u"Open website"_s, [] {
+                                           auto *n = new Notification;
+                                           n->setTitle(u"Memory leak"_s);
+                                           n->setText(u"Ok."_s);
+                                           n->send();
+                                       }}}));
     }
 
-    if (QStringLiteral("busy").startsWith(query.string()))
+    if (u"busy"_s.startsWith(query.string()))
     {
-        query.add(StandardItem::make({}, "busy", "Test delayed queries", "busy", icon, {}));
+        query.add(StandardItem::make({}, u"busy"_s, u"Test delayed queries"_s, u"busy"_s, icon, {}));
     }
 }
