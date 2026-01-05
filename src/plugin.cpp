10ee8246b1e09c1c957285960ce60ec93f4253b7
Manuel Schneider <manuelschneid3r@gmail.com>
2026-01-05 02:34:43 +0100

    fix: Workaround GCC13 bugs

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -14,7 +14,6 @@ using namespace Qt::StringLiterals;
 using namespace albert;
 using namespace std;
 
-
 Plugin::Plugin() { DEBG << "'Debug' created."; }
 
 Plugin::~Plugin() { DEBG << "'Debug' destroyed."; }
@@ -29,40 +28,42 @@ ItemGenerator Plugin::items(albert::QueryContext &ctx)
 {
     if (ctx.query() == u"busy"_s)
     {
-        for(int i = 0; ctx.isValid() && i < 3; ++i)
+        for (int i = 0; ctx.isValid() && i < 3; ++i)
         {
-            for (int nap = 0; nap < 100; ++nap){
+            for (int nap = 0; nap < 100; ++nap)
+            {
                 if (!ctx.isValid())
                     co_return;
                 this_thread::sleep_for(chrono::milliseconds(10));
             }
-            co_yield {StandardItem::make(u"debug"_s,
-                                         u"Item #%1"_s.arg(i),
-                                         u"Wow, Item #%1"_s.arg(i),
-                                         makeIcon)};
+            vector<shared_ptr<Item>> items{StandardItem::make(u"debug"_s,
+                                                              u"Item #%1"_s.arg(i),
+                                                              u"Wow, Item #%1"_s.arg(i),
+                                                              makeIcon)};
+            co_yield items;
         }
         co_return;
     }
 
     if (u"notification"_s.startsWith(ctx.query()))
     {
-        co_yield {StandardItem::make(u"debug"_s,
-                                     u"Memleaking notification"_s,
-                                     u"Leaks memory"_s,
-                                     makeIcon,
-                                     {{u"Debug"_s, u"Open website"_s, [] {
-                                           auto *n = new Notification;
-                                           n->setTitle(u"Memory leak"_s);
-                                           n->setText(u"Ok."_s);
-                                           n->send();
-                                       }}})};
+        vector<shared_ptr<Item>> items{StandardItem::make(u"debug"_s,
+                                                          u"Memleaking notification"_s,
+                                                          u"Leaks memory"_s,
+                                                          makeIcon,
+                                                          {{u"Debug"_s, u"Open website"_s, [] {
+                                                                auto *n = new Notification;
+                                                                n->setTitle(u"Memory leak"_s);
+                                                                n->setText(u"Ok."_s);
+                                                                n->send();
+                                                            }}})};
+        co_yield items;
     }
 
     if (u"busy"_s.startsWith(ctx.query()))
     {
-        co_yield {StandardItem::make(u"debug"_s,
-                                     u"busy"_s,
-                                     u"Test delayed queries"_s,
-                                     makeIcon)};
+        vector<shared_ptr<Item>> items{
+            StandardItem::make(u"debug"_s, u"busy"_s, u"Test delayed queries"_s, makeIcon)};
+        co_yield items;
     }
 }
